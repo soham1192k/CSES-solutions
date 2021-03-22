1b94d2b8440ba091a2dd5b52f842ecf3f9d76ef2
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-03-22 21:08:54 +0530

    Add files via upload

---
+++ Coin_Combinations_I.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int mod=1e9+7;
+const int mxN=10005;
+void solve(){
+    int n,x;cin>>n>>x;
+    vector<int>c(n);
+    for(int& x:c) cin>>x;
+    int dp[x+1];
+    memset(dp,0,sizeof(dp));
+    dp[0]=1;
+    for(int i=1;i<=x;i++){
+        for(int j=0;j<n;j++){
+            if(c[j]<=i)
+            dp[i]=(dp[i]+dp[i-c[j]])%mod;
+        }
+    }
+    cout<<dp[x];
+}  
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL); 
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
