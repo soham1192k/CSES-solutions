87b8a654da35cb7b4f3544d5908627f5eccb811b
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-03-22 19:31:21 +0530

    Add files via upload

---
+++ Bit_Strings.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int mod=1e9+7;
+const int mxN=10005;
+void solve(){
+    int n;cin>>n;   
+    int prod=1;
+    for(int i=1;i<=n;i++){
+        prod=(prod*2)%mod;
+    }
+    cout<<prod;
+}   
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL); 
+    int T=1;
+    // cin>>T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
